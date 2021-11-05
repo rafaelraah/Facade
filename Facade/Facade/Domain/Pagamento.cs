ebba3b38726ec768c02a49b807c5a3b175c729a0
Rafael <rafaelsantos.rsdn@gmail.com>
2021-11-05 17:09:35 -0300

    first commit

---
+++ Facade/Facade/Domain/Pagamento.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns
+{
+    public class Pagamento
+    {
+        public string Status { get; set; }
+        public decimal Valor { get; set; }
+        public MeioPagamento MeioPagamento { get; set; }
+
+        // Apenas para exemplificar, os possiveis meios de pagamento foram implementados direto 
+        // na classe de pagamento por questões de brevidade do exemplo
+        public string CartaoCredito { get; set; }
+        public string LinhaDigitavelBoleto { get; set; }
+        public string ConfirmacaoTransferencia { get; set; }
+    }
+}
---
+++ Facade/Facade/ExecucaoFacade.cs
@@ -0,0 +1,40 @@
+﻿using DesignPatterns;
+using System;
+using System.Collections.Generic;
+
+namespace Facade
+{
+    public class ExecucaoFacade
+    {
+        public static void Executar()
+        {
+            var produtos = new List<Produto>
+            {
+                new Produto{Nome = "Tenis Nike", Valor = new Random().Next(1000)},
+                new Produto{Nome = "Calça Adiddas", Valor = new Random().Next(1000)},
+                new Produto{Nome = "Bola de Basquete", Valor = new Random().Next(1000)}
+            };
+
+            var pedido = new Pedido
+            {
+                Id = Guid.NewGuid(),
+                Produtos = produtos
+            };
+
+            var pagamento = new Pagamento
+            {
+                CartaoCredito = "1111 2222 3333 4444"
+            };
+
+            // Resolver com DI
+            var pagamentoService = new PagamentoCartaoCreditoService(new PagamentoCartaoCreditoFacade(new PayPalGateway(), new ConfigurationManager()));
+
+            var pagamentoResult = pagamentoService.RealizarPagamento(pedido, pagamento);
+
+            Console.WriteLine(pagamentoResult.Status);
+
+            Console.ReadKey();
+
+        }
+    }
+}
